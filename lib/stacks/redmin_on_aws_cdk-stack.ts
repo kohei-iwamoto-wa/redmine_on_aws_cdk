23f557740f23535fce237fae2aa2842dbffb5a29
kohei.iwamoto <kohei.iwamoto17164@gmail.com>
2025-12-18 21:24:00 +0900

    ディレクトリ構成変更しました。

--- lib/stacks/redmin_on_aws_cdk-stack.ts
+++ lib/stacks/redmin_on_aws_cdk-stack.ts
@@ -1,12 +1,11 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { RedminVpc } from '../network/vpc';
+import { RedminVpc } from '../constructs/network/vpc';
 
 
 export class RedminOnAwsCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
     const vpc = new RedminVpc(this, 'RedminVPC', {});
   }
 }
